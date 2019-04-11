4c981fe0a36966e4edf6bba53a8ccdb8afc7b9b2
Stepan <stepan-alekseev-87@mail.ru>
2019-04-11 14:51:52 +0700

    L10 Вычмат в процессе коррекции

--- ВМ/L10Mine/src/L10Mine.java
+++ ВМ/L10Mine/src/L10Mine.java
@@ -1,16 +1,15 @@
-
-
 public class L10Mine {
-    static double a = 2;
-    static double b = 3;
-    static int Razb = 1000000;// число разбиений
+    static double a = 1.0;
+    static double b = 2.0;
+
+    static int Razb = 10;// число разбиений
     static int N = Razb, n = Razb / 2;
     static double h = (b - a) / N;
     static double H = (b - a) / n;
-    static double e = 0.0000001;
+    static double e = 0.00000001;// надо до 10 в минус восьмой
 
     public static double f(double x) {
-        return 2 * x;
+        return 1 / x;
     }
 
     public static double integrateTrapezii(double a, double b, double h) {
@@ -23,18 +22,17 @@ public class L10Mine {
         return sum;
     }
 
-    public static double integrateSimpson(double a, double b) {
-
-        // 1/3 terms
+    public static double integrateSimpson(double a, double b, double h, int N) {
+        // 1/3
         double sum = 1.0 / 3.0 * (f(a) + f(b));//
 
-        // 4/3 terms
+        // 4/3
         for (int i = 1; i < N - 1; i += 2) {
             double x = a + h * i;
             sum += 4.0 / 3.0 * f(x);
         }
 
-        // 2/3 terms
+        // 2/3
         for (int i = 2; i < N - 1; i += 2) {
             double x = a + h * i;
             sum += 2.0 / 3.0 * f(x);
@@ -44,21 +42,67 @@ public class L10Mine {
     }
 
     public static void main(String[] args) {
+        double aa, bb, dif ;
 
+        /*
+        System.out.println("\nTrapezii: ");
+        Razb = 10;// число разбиений
+        N = Razb;
+        n = Razb / 2;
+        h = (b - a) / N;
+        H = (b - a) / n;
         System.out.println("Trapezii(h) : " + integrateTrapezii(a, b, h));
         System.out.println("Trapezii(H) : " + integrateTrapezii(a, b, H));
-        double aa = integrateTrapezii(a, b, h);
-        double bb = integrateTrapezii(a, b, H);
-        double correct;
-        if (Math.abs(bb - aa) > 3.0 * e) {// если здесь д.б. цикл, то как его реализовать?
-            correct = aa + 1.0/3.0 * (aa-bb);
-            System.out.println("Correct = "+ correct);
-        }else System.out.println("aa = "+ aa);
-
-        while(Math.abs(aa - bb) > 3.0 * e){
-            aa = aa + 1.0/3.0 * Math.abs(aa-bb);
-            System.out.println("bb = "+bb);
+        aa = integrateTrapezii(a, b, h);
+        bb = integrateTrapezii(a, b, H);
+        dif = Math.abs(Math.abs(bb) - Math.abs(aa));
+        while (dif > e) {// если здесь д.б. цикл, то как его реализовать?
+            //correct = aa + 1.0 / 3.0 * (aa - bb);
+            dif = Math.abs(Math.abs(bb) - Math.abs(aa));
+            Razb *= 10.0;
+            N = Razb; //большее число разбиений
+            n = Razb / 2;//меньшее число разбиений
+            h = (b - a) / N;//меньший шаг
+            H = (b - a) / n;//больший шаг
+            aa = integrateTrapezii(a, b, h);
+            bb = integrateTrapezii(a, b, H);
+            System.out.println("    Correct = " + aa + ", h = " + h + "\n");
         }
-    }
+        // correct = correct + 1.0 / 3.0 * (aa - bb);
+        System.out.println("FinlCorrect = " + aa);
+        System.out.println("Точность : " + e);
+*/
 
+
+         aa = integrateSimpson(a, b, h, N);
+         bb = integrateSimpson(a, b, H, n);
+        double correct = 0, correct2 = 0;
+        System.out.println("\nSimpson: ");
+        dif = dif = Math.abs(Math.abs(bb) - Math.abs(aa));
+        System.out.println("Simpson(h) : " + aa);
+        System.out.println("Simpson(H) : " + bb);
+        while (dif > 3.0 * e) {// если здесь д.б. цикл, то как его реализовать?
+            dif = Math.abs(Math.abs(bb) - Math.abs(aa));
+            Razb += 10;
+            N = Razb; //большее число разбиений
+            n = Razb / 2;//меньшее число разбиений
+            h = (b - a) / N;//меньший шаг
+            H = (b - a) / n;//больший шаг
+            aa = integrateSimpson(a, b, h, N);
+            bb = integrateSimpson(a, b, H, n);
+            System.out.println("    Correct = " + aa + ", h = " + h + "\n");
+        }
+        // correct = correct + 1.0 / 3.0 * (aa - bb);
+        correct = aa + 1.0 / 3.0 * (aa - bb);
+        System.out.println("FinlCorrect = " + correct);
+        System.out.println("Точность : " + e);
+
+
+
+
+
+
+
+    }// ln(2) = 0.6931471805599
+    //Simpson при шаге 0.01 даёт погрешность десять в минус восьмой, трапеции тоже быстрые
 }
--- ВМ/L9TrigonInterp/src/L9Graphics.java
+++ ВМ/L9TrigonInterp/src/L9Graphics.java
@@ -9,70 +9,29 @@ import javafx.stage.Stage;
 public class L9Graphics extends Application {
     int n = 4; // n - число точек, по которым интерполируем
     int numD = 100;// число точек для точного графика( для корня из икс и остальных )
+    //int n = 30;//количество разбиений единичного отрезка
     double[] xx = new double[n];//точки по которым интерполируем
     double[] yy = new double[n];//точки по которым интерполируем
-    double[] hh = new double[n];//расстояния между иксами (только по икс)
-    double[] l = new double[n];// l[k] = (y[k] - y[k-1])/h[k]
-    double[] delta = new double[n];//прогоночные коэффициенты
-    double[] lambda = new double[n];//прогоночные коэффициенты
-    double[] c = new double[n];//коэффициенты в уравнениях для сплайнов
-    double[] d = new double[n];//коэффициенты в уравнениях для сплайнов
-    double[] b = new double[n];//коэффициенты в уравнениях для сплайнов
     double[] xc = new double[numD];//массив точек для записи координат графиков
     double[] yc = new double[numD];//массив точек для записи координат графиков
-    double[] j = new double[n - 1];
-    double[] A = new double[n - 1];
     double h;
-    double[] x = new double[n - 1];
-    double[] y = new double[n - 1];
     double pi = 3.1415926;
     double a[];
-    double ix[] = new double[n];
-    double iy[] = new double[n];
-
-    double B(int j) {
-        int ii;
-        double S = 0;
-        for (int i = 0; i < n; i++) {
-            ii = -n + i;
-            S = S + yy[i] * Math.sin(2 * pi * (double) (j * i) / (double) (2 * n + 1));
-        }
-        return 2 / (double) (2 * n + 1) * S;
-    }
-
-    double Interpolate(double x) {
-        double S = a[0];//b[i] по формулам начинается с единицы...
-        for (int i = 1; i < n; i++) {
-            S = S + a[i] * Math.cos((double) (i) * x) + b[i] * Math.sin((double) (i) * x);
-        }
-        return S;
-    }
-
-    double A(int j) {
-        double S = 0;
-        int ii;
-        for (int i = 0; i < n; i++) {
-
-            S = S + yy[i] * Math.cos(2 * pi * (double) (j) * (double) (i) / (2 * n + 1));
-        }
-        if (j == 0) return 1 / (double) (2 * n + 1) * S;
-
-        return 2 / (double) (2 * n + 1) * S;
-    }
 
     double AA(int j) {
         double S = 0;
         double ii = 1;
         for (int k = 0; k < n - 1; k++) {
 
-            S = S + yy[k] * Math.exp(-2 * pi * ii * (double) k * (double) j / (double) n);
+            S = S + yy[j] *
+                    Math.exp(-2 * pi * ii * (double) k *
+                            (double) j / (double) n);
         }
         return S / (double) n;
     }
-
     double Interpolate2(double x) {
         double S = 0;
-        double ii = -1;
+        double ii = 1;
         int c = 0, j = 0;
         while (x > xx[j + 1]) j++;// ищу нужный j
         h = 1 / (double) numD;// считаю шаг
@@ -80,7 +39,6 @@ public class L9Graphics extends Application {
         for (double t = (double) n / 2; t > (double) -n / 2; t--) {// пытаюсь реализовать цикл от n/2 до -n/2
             //наверное число в скобках(степень экспоненты д.б. отрицательным)
             S = S + a[j] * Math.exp(2 * pi * ii * t * (double) j * (x - xx[j]) / ((double) n * h));
-
         }
         return S;
     }
@@ -88,30 +46,27 @@ public class L9Graphics extends Application {
     public void countAll() {
         {
             a = new double[n];
-            b = new double[n];
-            System.out.println(" Коэффициенты a :");
+            System.out.println(" A :");
             for (int j = 0; j < n; j++) {
                 a[j] = AA(j);
-                System.out.println("a[j] = " + a[j]);
+                System.out.println("a["+j+"] = " + a[j]);
             }
 
-            //System.out.println(" Коэффициенты b :");
-            for (int j = 1; j < n; j++) {
-                b[j] = B(j);
-                //System.out.println("b[j] = " + b[j]);
-            }
             for (int jj = 0; jj < numD; jj++) {
                 int j = 0;
                 //if (j == 0) { xc[0] = xx[0]; } else
                 // xc[j] = xc[j - 1] + ((xx[n - 1] - xx[0]) / numD);
-                xc[jj] = xx[0] + (xx[n - 1] - xx[0]) / numD * jj;
+                xc[jj] =
+                        xx[0] +
+                                (xx[n - 1] -
+                                        xx[0]) / numD * jj;
                 // System.out.println("X: "+xc[j]);
                 yc[jj] = Interpolate2(xc[jj]);
             }
         }
     }
 
-    //инициализация интерполяционных точек//sin{x)
+    //инициализация интерполяционных точек//sin{x):
     public void initXXYY() {
         xx[0] = -4;
         yy[0] = 0.7;
@@ -121,13 +76,10 @@ public class L9Graphics extends Application {
         yy[2] = 0.17;
         xx[3] = 5.86;
         yy[3] = -0.4;
-        // x = xx.clone();
-        y = yy.clone();
-        //x[4] = 9;
-        //y[4] = 2;
-    }// 5 2
 
-    public void initXXYYе() {//инициализация интерполяционных точек//ДОМАШКА
+    }// 5 2
+    //инициализация интерполяционных точек//ДОМАШКА:
+    public void initXXYYе() {
         xx[0] = 2;
         yy[0] = 1;
         xx[1] = 4;
@@ -136,30 +88,7 @@ public class L9Graphics extends Application {
         yy[2] = 1;
         xx[3] = 8;
         yy[3] = 4;
-        // x = xx.clone();
-        y = yy.clone();
-        //x[4] = 9;
-        //y[4] = 2;
     }// 5 2
-
-    public void trig() {
-        A[0] = 0;
-        // k[0] = 0;
-        double h = 1 / n;
-        j[0] = -(n - 1) / 2;
-        x[0] = 0;
-        for (int i = 1; i < n - 1; i++) {
-            x[i] += x[i - 1] + 1 / n;
-        }
-        for (int k = 0; k < n - 1; k++) {
-            //A[0] += y[k] * Math.pow(Math.E, (-2 * Math.PI *  j * (x[j] / (double) n)));
-        }
-        A[0] *= (1 / n);
-    }
-
-    public void aCount() {
-        //int k
-    }
 
     public void initXXYYT() {//инициализация интерполяционных точек
         xx[0] = 1;
@@ -174,65 +103,10 @@ public class L9Graphics extends Application {
         yy[4] = 2;
     }// 5 2
 
-
-    public void coefficients() {
-        int k = 0;
-        //System.out.printf("\na[k]\t\tb[k]\t\tc[k]\t\td[k]\n");
-        for (k = 1; k < n; k++) {
-            //System.out.printf("%f\t%f\t%f\t%f\n", y[k], b[k], c[k], d[k]);
-        }
-    }
-
-    public void interpBuildCubSpl() {//заполняю массивы интерполированными точками
-        double start = xx[0];
-        double end = xx[n - 1];
-        double step = (end - start) / numD;
-        int i = 0;
-        for (double s = start; s <= end; s += step) {
-            int k;
-            for (k = 1; k < n; k++) {
-                if (s >= xx[k - 1] && s <= xx[k]) {
-                    break;//перескакиваем на нужное k
-                }
-            }
-            if (i < numD) {
-                yc[i] = yy[k] + b[k] * (s - xx[k]) + c[k] * Math.pow(s - xx[k], 2) + d[k] * Math.pow(s - xx[k], 3);//считаем значения функции с помощью сплайнов
-                xc[i] = s;
-                //System.out.printf("s = %f\t F = %f\n", s, yc[i]);
-                i++;
-            }
-        }
-    }
-
-    public void interpolate() {
-        int k = 0;
-        initXXYY();
-        for (k = 1; k < n; k++) {
-            hh[k] = xx[k] - xx[k - 1];//считаем расстояние(только по икс) между точками интерполяции
-            l[k] = (yy[k] - yy[k - 1]) / hh[k];//это из уравнения прямой, проходящей через две точки
-        }
-        delta[1] = -hh[2] / (2 * (hh[1] + hh[2]));//прогоночные коэффициенты
-        lambda[1] = 1.5 * (l[2] - l[1]) / (hh[1] + hh[2]);//прогоночные коэффициенты
-        for (k = 3; k < n; k++) {
-            delta[k - 1] = -hh[k] / (2 * hh[k - 1] + 2 * hh[k] + hh[k - 1] * delta[k - 2]);//прогоночные коэффициенты
-            lambda[k - 1] = (3 * l[k] - 3 * l[k - 1] - hh[k - 1] * lambda[k - 2]) /
-                    (2 * hh[k - 1] + 2 * hh[k] + hh[k - 1] * delta[k - 2]);//прогоночные коэффициенты
-        }
-        c[0] = 0;
-        c[n - 1] = 0;
-        for (k = n - 1; k >= 2; k--) {
-            c[k - 1] = delta[k - 1] * c[k] + lambda[k - 1];//находим коэффициенты с[k] по формулам обратной прогонки
-        }
-        for (k = 1; k < n; k++) {
-            d[k] = (c[k] - c[k - 1]) / (3 * hh[k]);//нахождение d[k] и b[k] по специальным формулам
-            b[k] = l[k] + (2 * c[k] * hh[k] + hh[k] * c[k - 1]) / 3;
-        }
-    }
-
     @Override
     public void start(Stage stage) {
 
-        stage.setTitle("Интерполяция кубическими сплайнами, тригонометрическая интерполяция");
+        stage.setTitle("Тригонометрическая интерполяция");
         final NumberAxis xAxis = new NumberAxis();
         final NumberAxis yAxis = new NumberAxis();
         xAxis.setLabel("X");
@@ -250,7 +124,7 @@ public class L9Graphics extends Application {
 
         countAll();
         XYChart.Series seriesTrig = new XYChart.Series();
-        for (int i = 0; i < numD * 0.55; i++) {
+        for (int i = 0; i < numD; i++) {
             seriesTrig.getData().add(new XYChart.Data(xc[i], yc[i]));
         }
         seriesTrig.setName("Тригонометрическая интерполяция");
