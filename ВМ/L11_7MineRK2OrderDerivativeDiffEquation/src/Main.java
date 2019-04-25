ba2325fd651b65f552ca37f2b9882186ab60f2f9
Stepan <stepan-alekseev-87@mail.ru>
2019-04-25 13:55:12 +0700

    Начал делать подогнанное задание

--- ВМ/L11_7MineRK2OrderDerivativeDiffEquation/src/Main.java
+++ ВМ/L11_7MineRK2OrderDerivativeDiffEquation/src/Main.java
@@ -11,8 +11,10 @@ import java.text.DecimalFormat;
 // то это значение (y(1)) и надо сравнивать для разных h?
 //Math.pow(x, 4)/12 + C - это интеграл взятый два раза от x^2
 //Какую часть массива сравнивать? Ведь получатся наборы, отличающиеся в два раза по количеству(при H и при h)
+//y(x0)=y0, y'(x0)=y0' должны быть даны для решения.
 public class Main extends Application {
     //решение системы дифференциальных уравнений методом Рунге-Кутты(Runge-Kutta-Nystrom)
+    //(точнее решение диффура второго порядка, приведнием его к системе из диффуров первого порядка)
     //как здесь применить интегрирование для представления g через z?
     //смогу ли я применить здесь интегрирование из 10-й лабы? Там то определённый интеграл...
     // ответы - это игреки?
@@ -26,14 +28,16 @@ public class Main extends Application {
     int k = 2, numD = 11;
     double x0, xn, y0, y1, z0, z1, h, H, xx[], yy[], YY[], eps = 10E-8;
     double k1, k2, k4, k3;
-    double q1, q2, q4, q3;
+    double k11, k22, k44, k33;
+
+    double x0F = 0, y0F = 1, z0F = 1, xnF = 2;
 
     public void start(Stage stage) {
-        System.out.println("X\t\t\t\tY");
-        x0 = 0;
-        xn = 0.5;
-        y0 = 0;
-        z0 = 0;
+        System.out.println("X\t\t\t\tY\t\t\t\tY'");
+        x0 = x0F;//зн-ие аргумента
+        xn = xnF;
+        y0 = y0F;//зн-ие ф-ции в точке x0
+        z0 = z0F;
         h = 0.1; // шаг
         H = 2 * h;
 
@@ -53,8 +57,6 @@ public class Main extends Application {
         System.out.println("RK4 launch with H = " + H);
         RK4(H, range, YY);
         System.out.println("i = " + i + " h = " + h);
-
-
 
 
         while (Math.abs(yy[RANGE - 1] - YY[range - 1]) > eps) {
@@ -125,44 +127,57 @@ public class Main extends Application {
         return (Math.cos(3 * x) - 4 * y);
     }
 
+    public double f2(double x, double y, double z) {
+        double result = x * x * x * x / 7;
+        return result;  //return Math.pow(x, 4) / 7;
+    }
     public double f(double x, double y, double z) {
-        double result = x * x * x * x / 12;
-        return result;  //return Math.pow(x, 4) / 12;
+        double result = (Math.exp(x)+y)/2;
+        return result;  //return Math.pow(x, 4) / 7;
     }
 
+    //функция, являющаяся y' ... может быть если сюда таки подставить реальную производную, то график нарисуется...
+    //(как я и представлял, т.е. если y'' = x^4/12, то нарисуется парабола...)
+    //вообще в примере на википедии сюда тупо задаётся известное численное значение первой производной
     public double g(double x, double y, double z) {
         return (z);
     }
-
+   /* public void Euhler6() {
+        yy[0] = y0;
+        xx[0] = x0;
+        double yc = y0, xc = x0;
+        //for (double xc = x0; i<range; xc += h) {
+        for (int i = 0; i < range; i++) {
+            if (i > 0) xc = xx[i] = xx[i - 1] + h;
+            y0 =  yy[i]  = y0 + h * f(xc, yc, z0);
+            System.out.println("xx[" + i + "] = " + xx[i]);
+            System.out.println("yy[" + i + "] = " + yy[i]);
+        }
+    }*/
     public void Euhler() {
         double yc = y0;
         for (double xc = x0; xc < xn; xc += h) {
             y1 = y0 + h * f(xc, yc, z0);
         }
     }
-
-    public void RK4(double h, int range, double[] yy) {
+    public void Euhler2(double h, int range, double[] yy){
         double yc = y0;
         double xc = x0;
-        //for (double xc = x0; xc < xn; xc += h) {
-        // проблема в том, что точки должны и увеличиться на h  и пройти от 0 до 1 за 10 шагов...
-        // какую переменную увеличивать на 0.1?
         for (int i = 0; i < range; i++) {
-            // xx[i] = xc;
             k1 = h * f(xc, yc, z0);
-            q1 = h * g(xc, yc, z0);
+            k11 = h * g(xc, yc, z0);
 
-            k2 = h * f(xc + h / 2.0, yc + q1 / 2.0, z0 + k1 / 2.0);
-            q2 = h * g(xc + h / 2.0, yc + q1 / 2.0, z0 + k1 / 2.0);
+            k2 = h * f(xc + h / 2.0, yc + k11 / 2.0, z0 + k1 / 2.0);
+            k22 = h * g(xc + h / 2.0, yc + k11 / 2.0, z0 + k1 / 2.0);
 
-            k3 = h * f(xc + h / 2.0, yc + q2 / 2.0, z0 + k2 / 2.0);
-            q3 = h * g(xc + h / 2.0, yc + q2 / 2.0, z0 + k2 / 2.0);
+            k3 = h * f(xc + h / 2.0, yc + k22 / 2.0, z0 + k2 / 2.0);
+            k33 = h * g(xc + h / 2.0, yc + k22 / 2.0, z0 + k2 / 2.0);
 
-            k4 = h * f(xc + h, yc + q3, z0 + k3);
-            q4 = h * g(xc + h, yc + q3, z0 + k3);
+            k4 = h * f(xc + h, yc + k33, z0 + k3);
+            k44 = h * g(xc + h, yc + k33, z0 + k3);
 
             z1 = z0 + (k1 + 2.0 * k2 + 2.0 * k3 + k4) / 6.0;
-            yy[i] = y1 = yc + (q1 + 2.0 * q2 + 2.0 * q3 + q4) / 6.0;
+            yy[i] = y1 = yc + (k11 + 2.0 * k22 + 2.0 * k33 + k44) / 6.0;
             //System.out.println("\t" + r(x0 + h, k) + "\t\t" + r(y1 ,k) + "\t\t" + r(z1 ,k));
 
             String x00 = new DecimalFormat("#0.0000000000000").format(xc);
@@ -170,6 +185,43 @@ public class Main extends Application {
             String z11 = new DecimalFormat("#0.0000000").format(z1);
 
             System.out.println(x00 + "\t" + y11 + "\t");
+            yc = y1;
+            xc += h;
+            z0 = z1;
+        }
+
+    }
+
+    public void RK4(double h, int range, double[] yy) {
+        double yc = y0;
+        double xc = x0;
+        //for (double xc = x0; xc < xn; xc += h) {
+        // проблема в том, что точки должны и увеличиться на h  и пройти от 0 до 1 за 10 шагов...
+        //это не надо скорее всего(10 шагов увеличатся и это окей)
+        // какую переменную увеличивать на 0.1?
+        for (int i = 0; i < range; i++) {
+            // xx[i] = xc;
+            k1 = h * f(xc, yc, z0);
+            k11 = h * g(xc, yc, z0);
+
+            k2 = h * f(xc + h / 2.0, yc + k11 / 2.0, z0 + k1 / 2.0);
+            k22 = h * g(xc + h / 2.0, yc + k11 / 2.0, z0 + k1 / 2.0);
+
+            k3 = h * f(xc + h / 2.0, yc + k22 / 2.0, z0 + k2 / 2.0);
+            k33 = h * g(xc + h / 2.0, yc + k22 / 2.0, z0 + k2 / 2.0);
+
+            k4 = h * f(xc + h, yc + k33, z0 + k3);
+            k44 = h * g(xc + h, yc + k33, z0 + k3);
+
+            z1 = z0 + (k1 + 2.0 * k2 + 2.0 * k3 + k4) / 6.0;
+            yy[i] = y1 = yc + (k11 + 2.0 * k22 + 2.0 * k33 + k44) / 6.0;
+            //System.out.println("\t" + r(x0 + h, k) + "\t\t" + r(y1 ,k) + "\t\t" + r(z1 ,k));
+
+            String x00 = new DecimalFormat("#0.0000000000000").format(xc);
+            String y11 = new DecimalFormat("#0.0000000000000").format(y1);
+            String z11 = new DecimalFormat("#0.0000000000000").format(z1);
+
+            System.out.println(x00 + "\t" + y11 + "\t" + z1);
             yc = y1;
             xc += h;
             z0 = z1;
